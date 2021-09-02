acebe46e7ef652464dcacb44ace28a6b4a55abca
Nikita Turkin <nexsanik@gmail.com>
2021-09-02 10:08:13 +0300

    fix: startup packages

--- backend/serviceAccountCreds.js
+++ backend/serviceAccountCreds.js
@@ -4,7 +4,7 @@ module.exports = {
   private_key_id: process.env.SERVICE_PRIVATE_KEY_ID,
   private_key: process.env.SERVICE_PRIVATE_KEY,
   client_email: "np-ttn@np-bot-278613.iam.gserviceaccount.com",
-  client_id: process.env.CLIENT_ID,
+  client_id: process.env.SERVICE_CLIENT_ID,
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
