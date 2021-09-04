4161afc946ae7cbd8c3612b5c2d8700cc2abf8c6
Nikita Turkin <nexsanik@gmail.com>
2021-09-04 19:34:29 +0300

    fix: service account

--- backend/serviceAccountCreds.js
+++ backend/serviceAccountCreds.js
@@ -1,13 +1,13 @@
 module.exports = {
   type: "service_account",
-  project_id: "np-bot-278613",
+  project_id: "sodium-castle-306011",
   private_key_id: process.env.SERVICE_PRIVATE_KEY_ID,
   private_key: process.env.SERVICE_PRIVATE_KEY.replace(/\\n/gm, "\n"),
-  client_email: "np-ttn@np-bot-278613.iam.gserviceaccount.com",
+  client_email: "black-list-bot@sodium-castle-306011.iam.gserviceaccount.com",
   client_id: process.env.SERVICE_CLIENT_ID,
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
   client_x509_cert_url:
-    "https://www.googleapis.com/robot/v1/metadata/x509/np-ttn%40np-bot-278613.iam.gserviceaccount.com",
+    "https://www.googleapis.com/robot/v1/metadata/x509/black-list-bot%40sodium-castle-306011.iam.gserviceaccount.com",
 };
